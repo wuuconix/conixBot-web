35192c3921b62c9ca75a4c4293acf1eca68ec850
wuuconix <1521900139@qq.com>
2022-05-25 16:19:29 +0800

    初始化

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# conixBot-web
-conixBot web端控制器
+# conixbot-web
+
+基于Mirai HTTP WebSocket Adapter 的网页版聊天
+
+目前完成了登录、获取群列表、获取群成员列表、发送群消息、发送群内临时消息的功能。
+
+接下来的工作主要是将websocket接受到的消息展示出来。
+
+![](https://tva1.sinaimg.cn/large/007YVyKcly1h2kq1a05j9j31080lm767.jpg)
---
+++ src/App.vue
@@ -0,0 +1,281 @@
+<template>
+    <div class="menu">
+        <!-- <img class="github" src="https://tva4.sinaimg.cn/large/007YVyKcly1h2kjmy9ccsj30u00u0gol.jpg"> -->
+        <div class="status">
+            <span v-if="online">在</span>
+            <span v-if="!online">离</span>
+            <img class="comi offline" src="https://tvax4.sinaimg.cn/large/007YVyKcly1h2jky1tq57g308e08ekjl.gif" v-if="!online">
+            <img class="comi online" src="https://tva4.sinaimg.cn/large/007YVyKcly1h2kl62sylyj30dp0fkjvq.jpg" v-if="online">
+            <span>线</span>
+        </div>
+        <el-dropdown trigger="click">
+            <el-button type="primary">选择群<el-icon class="el-icon--right"><arrow-down /></el-icon></el-button>
+            <template #dropdown>
+                <el-dropdown-menu>
+                    <el-dropdown-item v-for="group in groups" @click="chooseGroup(group)">{{ group["name"] }}</el-dropdown-item>
+                </el-dropdown-menu>
+            </template>
+        </el-dropdown>
+        <el-dropdown trigger="click">
+            <el-button type="primary">选择群成员<el-icon class="el-icon--right"><arrow-down /></el-icon></el-button>
+            <template #dropdown>
+                <el-dropdown-menu>
+                    <el-dropdown-item v-for="member in members" @click="chooseMember(member)">{{ member["memberName"] }}</el-dropdown-item>
+                </el-dropdown-menu>
+            </template>
+        </el-dropdown>
+        <el-button type="success" @click="openLoginDialog" v-if="!online">Login</el-button>
+        <el-button type="warning" @click="ws.close()" v-if="online">Logout</el-button>
+    </div>
+    <div class="chat">
+        <div class="chatname">{{ chatName }}</div>
+        <div class="msgspace"></div>
+        <div class="sendspace">
+            <div class="content" contenteditable ref="content"></div>
+            <el-button type="primary" class="send" @click="send">发送</el-button>
+        </div>
+    </div>
+    <el-dialog v-model="loginDialog" title="登录后使用conixBot" center draggable>
+        <img class="comi" src="https://tvax4.sinaimg.cn/large/007YVyKcly1h2jky1tq57g308e08ekjl.gif">
+        <el-input v-model="qq" placeholder="conixBot's qq" />
+        <el-input v-model="key" placeholder="conixBot's verify_key" show-password/>
+        <template #footer>
+            <span class="dialog-footer">
+                <el-button type="primary" @click="authenticate">Confirm</el-button>
+            </span>
+        </template>
+    </el-dialog>
+</template>
+
+<script>
+import { ElButton, ElDialog, ElInput, ElNotification, ElSkeleton, ElDropdown, ElDropdownMenu, ElDropdownItem, ElIcon } from 'element-plus'
+import { ArrowDown } from '@element-plus/icons-vue'
+import { wsBaseURI, qq, key } from '../config.js'
+import "element-plus/es/components/button/style/css"
+import "element-plus/es/components/dialog/style/css"
+import "element-plus/es/components/input/style/css"
+import "element-plus/es/components/notification/style/css"
+import "element-plus/es/components/skeleton/style/css"
+import "element-plus/es/components/dropdown/style/css"
+export default {
+    data() {
+        return {
+            loginDialog: false,
+            qq: qq ?? "",
+            key: key ?? "",
+            wsBaseURI: wsBaseURI ?? "",
+            ws: null,
+            online: false,
+            action: "listen",
+            groups: [],
+            group: {}, //选择的qq群
+            members: [],
+            merber: {}, //选择的qq群成员
+            chatType: "group", //聊天类型
+            chatName: "" //群名或者群昵称
+        }
+    },
+    components: { ElButton, ElDialog, ElInput, ElSkeleton, ElDropdown, ElDropdownMenu, ElDropdownItem, ArrowDown, ElIcon },
+    methods: {
+        openLoginDialog() {
+            this.loginDialog = true
+        },
+        authenticate() {
+            this.action = "login"
+            let socket = new WebSocket(`ws://${this.wsBaseURI}/message?verifyKey=${this.key}&qq=${this.qq}`)
+            socket.addEventListener("message", this.handle_message)
+            socket.addEventListener("close", this.handle_close)
+            this.ws = socket
+        },
+        handle_message(msg) {
+            let result = JSON.parse(msg.data)
+            console.log(result)
+            if (result.syncId == "-1") { //新消息
+                console.log("以上是新消息")
+            } else { //我们请求的回显
+                if (this.action == "login") {
+                    if (result.data.code == 0) {
+                        this.loginDialog = false
+                        this.online = true
+                        this.groupList() //登录成功后去查询qq群列表
+                        ElNotification({ title: 'Success', message: '验证通过！', type: 'success' })
+                    } else {
+                        ElNotification({ title: 'Error', message: '验证失败！', type: 'error' })
+                    }
+                } else if (this.action == "sendGroupMessage" || this.action == "sendTempMessage") {
+                    if (result.data.code == 0) {
+                        this.$refs.content.innerText = ""
+                        ElNotification({ title: 'Success', message: '发送成功!', type: 'success' })
+                    } else {
+                        ElNotification({ title: 'Error', message: '发送失败!', type: 'error' })
+                    }
+                } else if (this.action == "groupList") {
+                    if (result.data.code == 0) {
+                        this.groups = result.data.data
+                        this.action = "listen"
+                    }
+                } else if (this.action == "memberList") {
+                    if (result.data.code == 0) {
+                        this.members = result.data.data
+                        this.action = "listen"
+                    }
+                }
+            }
+
+        },
+        handle_close() {
+            this.online = false
+            ElNotification({ title: 'Success', message: '下线成功！', type: 'success' })
+        },
+        sendGroupMessage(content) {
+            this.action = "sendGroupMessage"
+            this.ws.send(JSON.stringify({
+                syncId: 114514,
+                command: "sendGroupMessage", // 命令字
+                subCommand: null,
+                content
+            }))
+        },
+        sendTempMessage(content) {
+            this.action = "sendTempMessage"
+            this.ws.send(JSON.stringify({
+                syncId: 114514,
+                command: "sendTempMessage", // 命令字
+                subCommand: null,
+                content
+            }))
+        },
+        groupList() {
+            this.action = "groupList"
+            this.ws.send(JSON.stringify({
+                syncId: 114514,
+                command: "groupList", // 命令字
+                subCommand: null,
+                content: null
+            }))
+        },
+        chooseGroup(group) {
+            this.chatType = "group"
+            this.group = group
+            this.chatName = group["name"]
+            this.memberList()
+        },
+        chooseMember(member) {
+            this.chatType = "member"
+            this.member = member
+            this.chatName = member["memberName"]
+        },
+        memberList() {
+            this.action = "memberList"
+            this.ws.send(JSON.stringify({
+                syncId: 114514,
+                command: "memberList", // 命令字
+                subCommand: null,
+                content: { target: this.group["id"] }
+            }))
+        },
+        send() {
+            if (this.chatType == "group") {
+                this.sendGroupMessage({ target: this.group["id"], messageChain:[{ type:"Plain", text: this.$refs.content.innerText }] })
+            } else {
+                this.sendTempMessage({ qq: this.member["id"], group: this.group["id"], messageChain:[{ type:"Plain", text: this.$refs.content.innerText }]})
+            }
+        }
+    }
+}
+</script>
+
+<style>
+    * {
+        padding: 0;
+        margin: 0;
+    }
+    html, body, div#app {
+        width: 100%;
+        height: 100%;
+    }
+    div.menu {
+        width: 100%;
+        height: 64px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: #337ecc;
+        box-sizing: border-box;
+        padding: 0px 5%;
+    }
+    img.github {
+        width: 50px;
+        border-radius: 25px;
+    }
+    img.comi {
+        width: 55px;
+        border-radius: 10px;
+    }
+    .el-input:nth-of-type(1) {
+        margin: 10px 0px;
+    }
+    @media screen and (min-width: 768px) {
+        .el-dialog {
+            width: 500px;
+        }
+    }
+
+    @media screen and (max-width: 768px) {
+        .el-dialog {
+            width: 80%;
+        }
+    }
+
+    .el-dialog--center .el-dialog__body {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+        padding: 15px;
+    }
+    div.status {
+        display: flex;
+        align-items: center;
+    }
+    div.status span {
+        margin: 0 10px;
+        color: #FFF;
+    }
+    div.chat {
+        width: 80%;
+        margin: auto;
+        height: calc(100% - 64px);
+        background-color: aquamarine;
+    }
+    div.chatname {
+        width: 100%;
+        height: 64px;
+        background-color:brown;
+        display: flex;
+        justify-content: center;
+        align-content: center;
+        line-height: 64px;
+    }
+    div.msgspace {
+        width: 100%;
+        height: calc(75% - 64px);
+        background-color:mediumslateblue;
+    }
+    div.sendspace {
+        width: 100%;
+        height: 25%;
+        background-color: gold;
+        position: relative;
+    }
+    .el-button.send {
+        position: absolute;
+        right: 0;
+        bottom: 0;
+    }
+    div.content {
+        width: 100%;
+        height: calc(100% - 32px);
+        background-color: greenyellow;
+    }
+</style>
